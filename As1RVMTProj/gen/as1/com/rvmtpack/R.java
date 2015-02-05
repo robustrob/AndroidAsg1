e1dfa2fbf82545b0c3773ef7439c3492d538d1ac
Massimo <kidmax14@hotmail.com>
2015-02-05 10:47:36 -0500

    v 0.2
    Layout Upgrades Name, ass1 2 and 3

--- As1RVMTProj/gen/as1/com/rvmtpack/R.java
+++ As1RVMTProj/gen/as1/com/rvmtpack/R.java
@@ -24,7 +24,19 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int EditText01=0x7f080005;
+        public static final int EditText02=0x7f080008;
+        public static final int EditText03=0x7f08000b;
+        public static final int TableRow01=0x7f080003;
+        public static final int TableRow02=0x7f080006;
+        public static final int TableRow03=0x7f080009;
+        public static final int TextView01=0x7f080004;
+        public static final int TextView02=0x7f080007;
+        public static final int TextView03=0x7f08000a;
+        public static final int action_settings=0x7f08000c;
+        public static final int editText1=0x7f080002;
+        public static final int tableRow1=0x7f080000;
+        public static final int textView1=0x7f080001;
     }
     public static final class layout {
         public static final int activity_as1_rvmt=0x7f030000;
@@ -35,7 +47,10 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int asg1=0x7f050003;
+        public static final int asg2=0x7f050004;
+        public static final int asg3=0x7f050005;
+        public static final int name=0x7f050002;
     }
     public static final class style {
         /** 
